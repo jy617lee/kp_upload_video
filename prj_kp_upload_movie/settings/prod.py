3aab5b0a8ddccb82274fc25a184cdb2dcc11c91d
jeeyunlee <jylee@kittenpla.net>
2017-08-22 00:44:06 +0900

    prod.py 데이터베이스 설정 수정

--- prj_kp_upload_movie/settings/prod.py
+++ prj_kp_upload_movie/settings/prod.py
@@ -5,10 +5,3 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "staticfiles") MEDIA_ROOT = os.path.join(BASE_DIR, "..", "media")
-
-DATABASES = { 'default': {
-  'ENGINE': 'django.db.backends.postgresql', 'NAME': 'ubuntu',
-  'USER': 'ubuntu',
-  'PASSWORD': 'withaskdjango!',
-  'HOST': '127.0.0.1', },
-}
