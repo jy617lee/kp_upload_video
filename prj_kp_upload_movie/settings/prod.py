0dac264ea1fa1b6a8bff27e43be44804b1e25968
jeeyunlee <jylee@kittenpla.net>
2017-08-22 09:46:30 +0900

    settings 모듈 지워보기

--- prj_kp_upload_movie/settings/prod.py
+++
@@ -1,7 +0,0 @@
-import os
-from .common import *
-
-DEBUG = False
-ALLOWED_HOSTS = ['*']
-
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "staticfiles") MEDIA_ROOT = os.path.join(BASE_DIR, "..", "media")
